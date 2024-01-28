e98e2b0da710fab5d2713f2d9d98fb62c2a0bcde
Flo <florianhofer024@gmail.com>
2024-01-28 09:58:21 +0100

    adding users to memories works

--- routes/memories.js
+++ routes/memories.js
@@ -24,6 +24,26 @@ router.get('/:memoryId/users', async (req, res) => {
     }
 });
 
+// Get created Memories by userId
+router.get('/createdMemories/:userId', async (req, res) => {
+    const userId = req.params.userId;
+  
+    try {
+      const [rows] = await db.query(`
+      SELECT * 
+      FROM memories
+      WHERE user_id = ?`, 
+      [userId]
+  );
+      if (rows.length > 0) {
+        res.json(rows); // Sending the first user found with that email
+      } else {
+        res.status(404).json({ message: 'User has not created any Memories yet' });
+      }
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+});
 // Retrieve details of a specific memory by memory ID
 router.get('/:memoryId', async (req, res) => {
     const memoryId = req.params.memoryId;
@@ -69,12 +89,12 @@ router.get('/:memoryID/:userID/friends', async (req, res) => {
 
 
 router.post('/createMemory', async (req, res) => {
-  const { creator_id, title, description, firestore_bucket_url, location_id, memory_date } = req.body;
+  const { creator_id, title, description, firestore_bucket_url, location_id, memory_date, title_pic } = req.body;
   try {
     // Insert the friendship request into the database
     const result = await db.query(
-      'INSERT INTO memories (user_id, title, text, image_url, location_id, memory_date) VALUES (?, ?, ?, ?, ?, ?)',
-      [creator_id, title, description, firestore_bucket_url, 1, memory_date]
+      'INSERT INTO memories (user_id, title, text, image_url, location_id, memory_date, title_pic) VALUES (?, ?, ?, ?, ?, ?, ?)',
+      [creator_id, title, description, firestore_bucket_url, 1, memory_date, title_pic]
     );
 
     const memoryId = result;
@@ -93,7 +113,6 @@ router.post('/addFriendsToMemory', async (req, res) => {
     for (const email of emails) {
       // Step 1: Get user_id from email
       const userResult = await db.query('SELECT user_id FROM users WHERE email = ?', [email]);
-      console.log(userResult[0][0]);
       if (userResult[0][0] != 0) {
         const userId = userResult[0][0].user_id;
 
