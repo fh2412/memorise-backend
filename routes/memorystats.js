7b1a2e2d2920382c229786986f0a796718e33790
Flo <florianhofer024@gmail.com>
2024-01-31 18:23:09 +0100

    created "createdthisyear" route

--- routes/memorystats.js
+++ routes/memorystats.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
 
-// Retrieve users by memory ID
+// Get total number of Memories created
 router.get('/created/:userId', async (req, res) => {
     const userd = req.params.userId;
   
@@ -23,5 +23,27 @@ router.get('/created/:userId', async (req, res) => {
     }
 });
 
+// Get number of Memories in this Year
+router.get('/createdthisyear/:userId', async (req, res) => {
+    const userd = req.params.userId;
+  
+    try {
+      const currentYear = new Date().getFullYear();
+
+      const rows = await db.query(`
+      SELECT count(memory_id) as count
+      FROM memories
+      WHERE user_id = ? AND YEAR(memory_date) = ?`, 
+      [userd, currentYear]
+  );
+      if (rows.length > 0) {
+        res.json(rows[0]); // Sending the first user found with that email
+      } else {
+        res.json({ message: 'No Memories created yet' });
+      }
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+});
 module.exports = router;
 
