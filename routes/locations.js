b85f3b2599f2ba36ed1afa11fe6451a5ee58edf1
Flo <florianhofer024@gmail.com>
2024-02-20 18:24:01 +0100

    added create location

--- routes/locations.js
+++ routes/locations.js
@@ -24,5 +24,24 @@ router.get('/:locId', async (req, res) => {
     }
 });
 
+
+router.post('/createLocation', async (req, res) => {
+    const { long, lat, l_country, l_city, l_street, l_postcode } = req.body;
+    try {
+      // Insert the friendship request into the database
+      const result = await db.query(
+        'INSERT INTO location ( longitude, latitude, country, city, street, postal_code) VALUES (?, ?, ?, ?, ?, ?)',
+        [long, lat, l_country, l_city, l_street, l_postcode]
+      );
+  
+      const locationId = result;
+  
+      res.json({ message: 'Location created successfully', locationId: locationId });
+    } catch (error) {
+      console.error('Error creating Location:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
 module.exports = router;
 
