98e06a2f10a3206be22b7ec587f2fc1728873de9
Flo <florianhofer024@gmail.com>
2024-05-16 18:05:17 +0200

    added .env file

--- config/db.js
+++ config/db.js
@@ -1,16 +1,14 @@
+require('dotenv').config();
+
 const mysql = require('mysql2');
 
 // Create a pool for handling MySQL connections
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: '127.0.0.1',
-  user: 'memorise_dev_user',
-  password: 'Wiispielen402',
-  database: 'memorise_gc_db'
-  /*user: 'memorise_dev_user', // e.g. 'my-db-user'
-  password: 'Wiispielen402', // e.g. 'my-db-password'
-  database: 'memorise_gc_db', // e.g. 'my-database'
-  socketPath: '/cloudsql/memorise-414417:europe-west3:memorise'*/
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
 // Export the pool to be used for queries
