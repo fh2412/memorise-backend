0bc677f8d251092c7b30c8b0f5c71500fe582d57
Flo <florianhofer024@gmail.com>
2024-07-25 17:02:07 +0200

    new create and edit routes for companies

--- routes/companies.js
+++ routes/companies.js
@@ -24,6 +24,35 @@ router.get('/:userId', async (req, res) => {
     }
 });
 
+// Create a new company
+router.post('/create/:userId', (req, res) => {
+  const userId = req.params.userId;
+
+  const { name, phone, email, website } = req.body;
+  const query = 'INSERT INTO companies (name, phone, email, website, created_at, owner_id) VALUES (?, ?, ?, ?, NOW(), ?)';
+  db.query(query, [name, phone, email, website, userId], (error, results) => {
+      if (error) {
+          return res.status(500).json({ error: 'Database query error' });
+      }
+      res.json({ id: results.insertId, name, phone, email, website });
+  });
+});
+
+// Update an existing company
+router.put('/update/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, phone, email, website } = req.body;
+  const query = 'UPDATE companies SET name = ?, phone = ?, email = ?, website = ? WHERE id = ?';
+  db.query(query, [name, phone, email, website, id], (error, results) => {
+      if (error) {
+          return res.status(500).json({ error: 'Database query error' });
+      }
+      if (results.affectedRows === 0) {
+          return res.status(404).json({ error: 'Company not found' });
+      }
+      res.status(200).json({ id, name, phone, email, website });
+  });
+});
 
 //Leave Company
 router.put('/leave/:id', async (req, res) => {
@@ -43,6 +72,23 @@ router.put('/leave/:id', async (req, res) => {
     }
 });
 
+//SET emplyee TODO
+router.put('/leave/:id', async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const updateUserQuery = `
+      UPDATE users 
+      SET company_id = ?
+      WHERE user_id = ?
+    `;
+    await db.query(updateUserQuery, [null, userId]);
+
+    res.status(200).json({ message: 'User left company successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 //Delete Company
 router.delete('/delete/:id', async (req, res) => {
