20e44d59ae910b695fbc8c852bd9ff2c812907cb
Flo <florianhofer024@gmail.com>
2024-09-28 09:06:24 +0200

    npm audit fix

--- routes/image-gallery.js
+++ routes/image-gallery.js
@@ -2,6 +2,8 @@ const express = require('express');
 const admin = require('firebase-admin');
 const router = express.Router();
 
+const { ref, listAll, getDownloadURL } = require('firebase/storage');
+
 var serviceAccount = require("A:/programming/memorise-910c3-firebase-adminsdk-c4phi-bb250db9f3.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -9,30 +11,34 @@ admin.initializeApp({
 
 const storage = admin.storage();  
 
-
 // Route to retrieve the first 5 images
 router.get('/previewimages/:folderId', async (req, res) => {
-    const folderId = req.params.folderId;
-  
-    try {
-      const listRef = ref(storage, `memories/${folderId}`);
-      const { items } = await listAll(listRef);
-  
-      const previewImages = items.slice(0, 5); // Get the first 5 items
-  
-      const imageUrls = await Promise.all(
-        previewImages.map(async (itemRef) => {
-          const url = await getDownloadURL(ref(storage, itemRef.fullPath));
-          return url;
-        })
-      );
-  
-      res.json(imageUrls); // Send array of image URLs as JSON response
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Error fetching image URLs'); // Handle errors
-    }
-  });
+  const folderId = req.params.folderId;
+  try {
+    console.log(storage, '/memories', folderId);
+    const listRef = ref(storage, `memories/${folderId}`);
+    
+    // Check if the folder exists and has items
+    const listResult = await listAll(listRef);
+    console.log("Listresult:", listResult);
+
+    const previewImages = listResult.items.slice(0, 5); // Get the first 5 items
+
+    const imageUrls = await Promise.all(
+      previewImages.map(async (itemRef) => {
+        const url = await getDownloadURL(itemRef); // Use itemRef directly
+        return url;
+      })
+    );
+
+    res.json(imageUrls); // Send array of image URLs as JSON response
+  } catch (error) {
+    console.error('Error fetching image URLs:', error); // Improved error logging
+    res.status(500).send('Error fetching image URLs');
+  }
+});
+
+
 
 
 //GET all images with dimensions
@@ -68,6 +74,15 @@ router.get('/images/:folderId', async (req, res) => {
     }
   });
 
+  router.get('/gallerytest', async (req, res) => {
+  
+    try {
+      res.json("Success!");
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error fetching and classifying images');
+    }
+  });
 
 
 module.exports = router;
