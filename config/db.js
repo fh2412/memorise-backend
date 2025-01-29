c721fe5ef091b994f752e930b2398d451e74f9c3
Florian Hofer <147911748+fh2412@users.noreply.github.com>
2025-01-29 17:13:46 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+/*require('dotenv').config();
 
 const mysql = require('mysql2');
 
@@ -13,5 +13,28 @@ const pool = mysql.createPool({
 
 // Export the pool to be used for queries
 
+
+module.exports = pool.promise();*/
+
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const dbConfig = {
+  connectionLimit: 10,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+};
+
+// Add Cloud SQL socket path if running in production
+if (process.env.INSTANCE_CONNECTION_NAME) {
+  dbConfig.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+} else {
+  // Use TCP connection for local development
+  dbConfig.host = process.env.DB_HOST || 'localhost';
+  dbConfig.port = process.env.DB_PORT || 3306;
+}
+
+const pool = mysql.createPool(dbConfig);
 
 module.exports = pool.promise();
