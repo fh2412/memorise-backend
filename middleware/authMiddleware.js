48e369858998e9526c0ce8d41e8dcd0163c900fc
Flo <florianhofer024@gmail.com>
2025-02-17 17:15:42 +0100

    changed authMiddleware to use init firebase

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-const admin = require('firebase-admin');
+const { getFirebaseAdmin } = require('../config/firebaseAdmin');
 const logger = require('./logger');
 
 /**
@@ -6,39 +6,40 @@ const logger = require('./logger');
  * @description Verifies the token and adds user data to the request object
  */
 const authenticateFirebaseToken = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  const clientIp = req.ip || req.connection.remoteAddress;
+    const authHeader = req.headers.authorization;
+    const clientIp = req.ip || req.connection.remoteAddress;
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-      const error = new Error('Unauthorized: No token provided');
-      error.status = 401; // Unauthorized
-      error.unauthorized = true; // Optional: Mark as an unauthorized error
-      logger.error({
-          message: error.message,
-          url: req.originalUrl,
-          method: req.method,
-          ip: clientIp,
-      });
-      return next(error); // Pass the error to next()
-  }
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        const error = new Error('Unauthorized: No token provided');
+        error.status = 401; // Unauthorized
+        error.unauthorized = true; // Optional: Mark as an unauthorized error
+        logger.error({
+            message: error.message,
+            url: req.originalUrl,
+            method: req.method,
+            ip: clientIp,
+        });
+        return next(error); // Pass the error to next()
+    }
 
-  const token = authHeader.split(' ')[1];
+    const token = authHeader.split(' ')[1];
 
-  try {
-      const decodedToken = await admin.auth().verifyIdToken(token);
-      req.user = decodedToken;
-      next(); // Success, continue to the next middleware/route
-  } catch (error) {
-      const authError = new Error('Unauthorized: Invalid token'); // Create a new error
-      authError.status = 401;
-      authError.unauthorized = true; // Optional: Mark as an unauthorized error
-      logger.error(`Error verifying Firebase token: ${error}`);
+    try {
+        const admin = getFirebaseAdmin();
+        const decodedToken = await admin.auth().verifyIdToken(token);
+        req.user = decodedToken;
+        next(); // Success, continue to the next middleware/route
+    } catch (error) {
+        const authError = new Error('Unauthorized: Invalid token'); // Create a new error
+        authError.status = 401;
+        authError.unauthorized = true; // Optional: Mark as an unauthorized error
+        logger.error(`Error verifying Firebase token: ${error}`);
 
-      // Optionally, you can pass the original Firebase error for more details in your error handler:
-      authError.firebaseError = error;
+        // Optionally, you can pass the original Firebase error for more details in your error handler:
+        authError.firebaseError = error;
 
-      next(authError); // Pass the error to next()
-  }
+        next(authError); // Pass the error to next()
+    }
 };
 
 module.exports = authenticateFirebaseToken;
