2532d91ae70424198d3a687c56875ac80e76b355
fh2412 <florianhofer024@gmail.com>
2025-02-20 08:16:03 +0100

    roleback

--- config/db.js
+++ config/db.js
@@ -20,17 +20,3 @@ if (process.env.INSTANCE_CONNECTION_NAME) {
 const pool = mysql.createPool(dbConfig);
 
 module.exports = pool.promise();
-/*
-
-const dbConfig = {
-  connectionLimit: 10,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 3306,
-};
-
-// Remove the socketPath logic unless needed elsewhere
-const pool = mysql.createPool(dbConfig);
-module.exports = pool.promise();*/
